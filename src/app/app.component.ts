b83de8d61cba530fc993e663bb9eb3c898b87aa0
adeojoemmanuel <emmanuel.adojo@yahoo.com>
2017-09-18 16:40:21 +0100

    api included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 /*import { HomePage } from '../pages/home/home';*/
 import { LoginPage } from '../pages/login/login';
+import { BuyDataPage } from '../pages/buy-data/buy-data';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = BuyDataPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
